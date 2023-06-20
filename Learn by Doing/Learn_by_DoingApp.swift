cc4a9da2c1fdb32413607692dd09c5cc76f4a797
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-20 16:29:01 +0900

    Initial Commit

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by 飯塚政美 on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
