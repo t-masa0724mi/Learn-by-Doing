b2939cf71e8f0660f50398c942412083a8a692c1
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-21 13:22:09 +0900

    ボタンタップ時のサウンド追加

--- Learn by Doing/CardView.swift
+++ Learn by Doing/CardView.swift
@@ -23,6 +23,8 @@ struct CardView: View {
             
             Button(action: {
                 print("Button was tapped,")
+                
+                playSound(sound: "sound-chime", type: "mp3")
             }) {
                 HStack {
                     Text(card.callToAction)
